7257251cf9617afc61ddfd4b0d30e41b8ee0ed7a
kyce-harper <kyceharper25@icloud.com>
2025-04-28 09:04:25 -0700

    Signed-off-by: kyce-harper <kyceharper25@icloud.com>
    Initial Commit
    
    lrkgnue
    clear
    
    svkjdb
    \agklnewg

---
+++ Siply_HydrationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Siply_HydrationTrackerApp.swift
+//  Siply-HydrationTracker
+//
+//  Created by Kyce Harper on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Siply_HydrationTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
